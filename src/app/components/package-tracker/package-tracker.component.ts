bbfd438d017edcc5f7219bcc57f173db35d4c5ea
Ajayi Taiwo <ajayitaiwo89@yahoo.com>
2024-07-11 11:55:50 +0100

    optimized codebase

--- src/app/components/package-tracker/package-tracker.component.ts
+++ src/app/components/package-tracker/package-tracker.component.ts
@@ -9,40 +9,58 @@ declare var google: any;
   templateUrl: './package-tracker.component.html',
   styleUrls: ['./package-tracker.component.css']
 })
-export class PackageTrackerComponent implements OnInit {
+export class PackageTrackerComponent implements OnInit, OnDestroy {
   packageId: string = '';
-  package: any;
-  delivery: any;
-  deliveries: any[] = [];
+  packageDetails: any = null;
+  deliveryDetails: any = null;
   map: any;
-  markers: any = {};
+  currentLocationMarker: any;
+  sourceMarker: any;
+  destinationMarker: any;
 
-  @ViewChild('map', { static: true })
-  mapElement!: ElementRef;
-
-  constructor(private apiService: ApiService, private socketService: SocketService) { }
+  constructor(private apiService: ApiService, private socketService: SocketService) {}
 
   ngOnInit(): void {
+    this.initializeMap();
     this.socketService.onEvent('delivery_updated').subscribe((updatedDelivery) => {
-      if (this.delivery && this.delivery.delivery_id === updatedDelivery.delivery_id) {
-        this.delivery = updatedDelivery;
-        this.updateMarker('current', updatedDelivery.location);
+      if (this.deliveryDetails && this.deliveryDetails.delivery_id === updatedDelivery.delivery_id) {
+        this.deliveryDetails = updatedDelivery;
+        this.updateMarkers();
       }
     });
-    this.initializeMap();
   }
 
-  updateMarker(type: string, position: { lat: number, lng: number }) {
-    if (!this.markers[type]) {
-      this.markers[type] = new google.maps.Marker({
-        map: this.map,
-        position: new google.maps.LatLng(position.lat, position.lng),
-        label: type.charAt(0).toUpperCase(),
-      });
-    } else {
-      this.markers[type].setPosition(new google.maps.LatLng(position.lat, position.lng));
-    }
+  ngOnDestroy(): void {}
+
+  trackPackage() {
+    this.apiService.getPackageByPackageId(this.packageId).subscribe(
+      (pkg) => {
+        this.packageDetails = pkg;
+        if (this.packageDetails.active_delivery_id) {
+          this.fetchDelivery(this.packageDetails.active_delivery_id);
+        }
+      },
+      (error) => {
+        console.error('Package not found', error);
+        this.packageDetails = null;
+        this.deliveryDetails = null;
+      }
+    );
   }
+
+  fetchDelivery(deliveryId: string) {
+    this.apiService.getDeliveryById(deliveryId).subscribe(
+      (delivery) => {
+        this.deliveryDetails = delivery[0];
+        this.updateMarkers();
+      },
+      (error) => {
+        console.error('Delivery not found', error);
+        this.deliveryDetails = null;
+      }
+    );
+  }
+
 
   initializeMap() {
     const mapProperties = {
@@ -50,26 +68,45 @@ export class PackageTrackerComponent implements OnInit {
       zoom: 5,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     };
-    this.map = new google.maps.Map(this.mapElement.nativeElement, mapProperties);
+    this.map = new google.maps.Map(document.getElementById('map'), mapProperties);
   }
 
-  trackPackage(): void {
-    this.apiService.getPackageByPackageId(this.packageId).subscribe((deliveryPackage) => {
-      if (deliveryPackage) {
-        this.package = deliveryPackage;
-        if (this.package.active_delivery_id) {
-          this.loadDeliveryDetails(this.package.active_delivery_id);
-        }
+  updateMarkers() {
+    if (this.deliveryDetails) {
+      const source = this.packageDetails.from_location || { lat: 0, lng: 0 };
+      const destination = this.packageDetails.to_location || { lat: 0, lng: 0 };
+      const location = this.deliveryDetails.location || { lat: 0, lng: 0 };
+
+      if (!this.sourceMarker) {
+        this.sourceMarker = new google.maps.Marker({
+          map: this.map,
+          position: new google.maps.LatLng(source.lat, source.lng),
+          label: 'S'
+        });
+      } else {
+        this.sourceMarker.setPosition(new google.maps.LatLng(source.lat, source.lng));
       }
-    });
-  }
 
-  loadDeliveryDetails(active_delivery_id: string) {
-    this.apiService.getDeliveryById(active_delivery_id).subscribe(delivery => {
-      this.delivery = delivery;
-      this.updateMarker('current', this.delivery[0].location);
-      this.socketService.emitEvent('track_delivery', { delivery_id: this.delivery[0].delivery_id });
-    });
-  }
+      if (!this.destinationMarker) {
+        this.destinationMarker = new google.maps.Marker({
+          map: this.map,
+          position: new google.maps.LatLng(destination.lat, destination.lng),
+          label: 'D'
+        });
+      } else {
+        this.destinationMarker.setPosition(new google.maps.LatLng(destination.lat, destination.lng));
+      }
 
+      if (!this.currentLocationMarker) {
+        this.currentLocationMarker = new google.maps.Marker({
+          map: this.map,
+          position: new google.maps.LatLng(location.lat, location.lng),
+          label: 'C'
+        });
+      } else {
+        this.currentLocationMarker.setPosition(new google.maps.LatLng(location.lat, location.lng));
+      }
+      this.map.setCenter(new google.maps.LatLng(location.lat, location.lng));
+    }
+  }
 }
